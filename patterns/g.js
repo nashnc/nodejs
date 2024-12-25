68e44831ea2d6608776c05ce2a0a3e562af41506
AviNash N C <31386636+nashnc@users.noreply.github.com>
2024-12-25 08:11:30 +0530

    2024-12-25 @ 08:11 AM

--- patterns/g.js
+++ patterns/g.js
@@ -2,18 +2,24 @@ var PromptSync = require("prompt-sync")();
 
 var size = PromptSync(`enter the size \n`);
 pattern = "";
-hSize = Math.round(size / 2);
+halfSize = Math.round(size / 2);
+remainder = size % 2;
+
 for (i = 0; i < size; i++) {
   for (j = 0; j < size; j++) {
     if (i == 0 || i == size - 1 || j == 0) {
       pattern += "* ";
-    } else if (j == size - 1 && i >= hSize - 1 && size % 2 != 0) {
+    } else if (j == size - 1 && i >= halfSize - 1 && remainder != 0) {
       pattern += "* ";
-    } else if (j == size - 1 && i >= hSize && size % 2 == 0) {
+    } else if (j == size - 1 && i >= halfSize && remainder == 0) {
       pattern += "* ";
-    } else if (size % 2 != 0 && i == hSize - 1 && (j == i || j > hSize - 1)) {
+    } else if (
+      remainder != 0 &&
+      i == halfSize - 1 &&
+      (j == i || j > halfSize - 1)
+    ) {
       pattern += "* ";
-    } else if (size % 2 == 0 && i == hSize && (j == i || j > hSize)) {
+    } else if (remainder == 0 && i == halfSize && (j == i || j > halfSize)) {
       pattern += "* ";
     } else {
       pattern += "  ";
